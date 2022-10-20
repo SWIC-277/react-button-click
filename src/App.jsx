466245272cc53f40448e80deaf0e3f4d2674504a
Manav Misra <manavm@visionwebsoft.com>
2022-10-19 19:10:27 -0500

    Add unique key for ButtonCounter

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import ButtonCounter from "./components/ButtonCounter";
 
 const buttons = [
   {
+    id: 1,
     txt: "Button 1",
   },
   {
+    id: 2,
     txt: "Button 2",
   },
   {
+    id: 3,
     txt: "Button 3",
   },
 ];
@@ -20,8 +23,8 @@ function App() {
         Button Counter
       </h1>
 
-      {buttons.map(({ txt }) => (
-        <ButtonCounter txt={txt} />
+      {buttons.map(({ txt, id }) => (
+        <ButtonCounter txt={txt} key={id} />
       ))}
     </>
   );
